fcc3ef0b881983b5019e26f4b018b522920f473e
KenRouKoro <koro@korostudio.cn>
2022-06-04 17:30:54 +0800

    2.0.0

---
+++ koroworld-lib/src/main/java/cn/korostudio/koroworld/lib/KoroWorldLib.java
@@ -0,0 +1,14 @@
+package cn.korostudio.koroworld.lib;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KoroWorldLib implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("KoroWorld-Lib");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("KoroWorld-Lib Is Loaded!");
+	}
+}
